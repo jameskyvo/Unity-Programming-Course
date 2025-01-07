8e2801c787e1dd83869ef1fa434e5e91abfa6ca5
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2025-01-06 22:31:16 -0600

    Completed bonus challenge 2
    Added tanks and tank ai

---
+++ Assets/Course Library/Scripts/TankAI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
+
+public class TankAI : MonoBehaviour
+{
+    public float speed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
